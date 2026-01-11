8ebf19a8a46e603eed1c0d7c9085d15aeaa4fd66
sathyarajsp09-blip <sathyarajsp09@gmail.com>
2026-01-11 18:39:25 +0530

    Fix DB initialization for production

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app.secret_key = "secret123"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 
 # =====================
 # MODELS
@@ -133,6 +135,4 @@ def logout():
 # APP START
 # =====================
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run()
